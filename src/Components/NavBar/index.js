69b6f0f288206815aed0a18aad4d4fe666551914
ystheprince <yungsalim@me.com>
2019-11-06 19:57:38 -0500

    updated the navbar file

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -11,11 +11,11 @@ export default function NavBars() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/landing">Home</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
             <NavDropdown title="Products" id="basic-nav-dropdown">
               <NavDropdown.Item href="/men">Men</NavDropdown.Item>
               <NavDropdown.Item href="/women">Women</NavDropdown.Item>
-              <NavDropdown.Item href="/landing">All</NavDropdown.Item>
+              <NavDropdown.Item href="/home">All</NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
             { window.location.href.includes('admin') && <Nav.Link href="/admin/orders">Orders</Nav.Link> }
